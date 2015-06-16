ada2c92843561c6608de8e195c7480437cbee89e
Roger <wenrui@gmail.com>
2015-06-16 10:28:15 +0800

    support nwjc

--- src/compiler.cc
+++ src/compiler.cc
@@ -1136,6 +1136,7 @@ bool Compiler::Analyze(ParseInfo* info,
 }
 
 bool Compiler::ParseAndAnalyze(ParseInfo* info) {
+  if (info->script()->source() == info->isolate()->heap()->undefined_value()) return false;
   if (!parsing::ParseAny(info)) return false;
   if (info->is_toplevel()) EnsureSharedFunctionInfosArrayOnScript(info);
   if (!Compiler::Analyze(info)) return false;
@@ -1525,7 +1526,8 @@ Handle<SharedFunctionInfo> Compiler::GetSharedFunctionInfoForScript(
     maybe_result = compilation_cache->LookupScript(
         source, script_name, line_offset, column_offset, resource_options,
         context, language_mode);
-    if (maybe_result.is_null() && FLAG_serialize_toplevel &&
+    //NWJS#5168: will hit previous cache, use 0 source_length trick to bypass
+    if ((maybe_result.is_null() || !source_length) && FLAG_serialize_toplevel &&
         compile_options == ScriptCompiler::kConsumeCodeCache &&
         !isolate->debug()->is_loaded()) {
       // Then check cached code provided by embedder.
