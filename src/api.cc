da13fcda649cd9fd2daec0fd55ce2edecaa5f21b
Roger <wenrui@gmail.com>
2015-06-16 10:28:15 +0800

    support nwjc

--- src/api.cc
+++ src/api.cc
@@ -10100,6 +10100,15 @@ void Testing::DeoptimizeAll(Isolate* isolate) {
 }
 
 
+void FixSourceNWBin(Isolate* v8_isolate, Local<UnboundScript> script) {
+  i::Isolate* isolate = reinterpret_cast<i::Isolate*>(v8_isolate);
+  i::Handle<i::HeapObject> obj =
+    i::Handle<i::HeapObject>::cast(v8::Utils::OpenHandle(*script));
+  i::Handle<i::SharedFunctionInfo>
+      function_info(i::SharedFunctionInfo::cast(*obj), obj->GetIsolate());
+  reinterpret_cast<i::Script*>(function_info->script())->set_source(isolate->heap()->undefined_value());
+}
+
 namespace internal {
 
 
