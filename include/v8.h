3d0b59c904ede15041ce9e23836e5755b843dc83
Roger Wang <roger.wang@intel.com>
2015-08-19 14:53:28 +0800

    expose array_buffer_allocator

--- include/v8.h
+++ include/v8.h
@@ -5260,6 +5260,8 @@ class V8_EXPORT PersistentHandleVisitor {  // NOLINT
  */
 class V8_EXPORT Isolate {
  public:
+  ArrayBuffer::Allocator* array_buffer_allocator();
+  
   /**
    * Initial configuration parameters for a new Isolate.
    */
