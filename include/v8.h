59c66f12a25e1ad517c2041a263cb101376216c5
Roger Wang <roger.wang@intel.com>
2015-08-19 14:53:28 +0800

    expose array_buffer_allocator

--- include/v8.h
+++ include/v8.h
@@ -6523,6 +6523,8 @@ typedef DeserializeInternalFieldsCallback DeserializeEmbedderFieldsCallback;
  */
 class V8_EXPORT Isolate {
  public:
+  ArrayBuffer::Allocator* array_buffer_allocator();
+  
   /**
    * Initial configuration parameters for a new Isolate.
    */
