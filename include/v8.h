74410dc7a25d4ce95597523971688b77c8f81ce0
Roger Wang <roger.wang@intel.com>
2015-08-19 14:53:28 +0800

    expose array_buffer_allocator

--- include/v8.h
+++ include/v8.h
@@ -5588,6 +5588,8 @@ class V8_EXPORT EmbedderHeapTracer {
  */
 class V8_EXPORT Isolate {
  public:
+  ArrayBuffer::Allocator* array_buffer_allocator();
+  
   /**
    * Initial configuration parameters for a new Isolate.
    */
