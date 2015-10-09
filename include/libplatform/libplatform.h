8d88caba116ade50adaba50d08f045c1557f7f18
Roger Wang <roger.wang@intel.com>
2015-10-09 13:49:43 +0800

    export platform functions on Linux

--- include/libplatform/libplatform.h
+++ include/libplatform/libplatform.h
@@ -19,8 +19,7 @@ namespace platform {
  * of zero is passed, a suitable default based on the current number of
  * processors online will be chosen.
  */
-v8::Platform* CreateDefaultPlatform(int thread_pool_size = 0);
-
+v8::Platform* V8_EXPORT2 CreateDefaultPlatform(int thread_pool_size = 0);
 
 /**
  * Pumps the message loop for the given isolate.
@@ -30,7 +29,7 @@ v8::Platform* CreateDefaultPlatform(int thread_pool_size = 0);
  * not block if no task is pending. The |platform| has to be created using
  * |CreateDefaultPlatform|.
  */
-bool PumpMessageLoop(v8::Platform* platform, v8::Isolate* isolate);
+bool V8_EXPORT2 PumpMessageLoop(v8::Platform* platform, v8::Isolate* isolate);
 
 /**
  * Attempts to set the tracing controller for the given platform.
