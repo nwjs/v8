5013a14d01e607f5b26ec8e287a8c6ec99576210
Roger Wang <roger@nwjs.io>
2018-11-30 18:54:54 -0700

    Update to 71.0.3578.80: b14fdae7799e2990dd49d7c6d933ca4928b936ef

--- test/mjsunit/regress/regress-crbug-90771.js
+++ test/mjsunit/regress/regress-crbug-90771.js
@@ -2,13 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+
 // Flags: --allow-natives-syntax
+function target() {};
 
-function f() {
-  return arguments.length;
+for (let key of Object.getOwnPropertyNames(this)) {
+  try {
+    let newTarget = this[key];
+    let arg = target;
+    Reflect.construct(target, arg, newTarget);
+  } catch {}
 }
-
-var a = [];
-%OptimizeFunctionOnNextCall(f);
-a.length = 81832;
-f(...a);
